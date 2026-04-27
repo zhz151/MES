9b424c2b100f27623eefa0690febf412725492d0
zhz151 <12248151@qq.com>
2026-04-28 04:54:58 +0800

    mmmm

---
+++ MES.Data/Migrations/20260427202346_FixStatusColumnsToString.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MES.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixStatusColumnsToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "WorkOrder",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "NotGenerated",
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ChangeType",
+                table: "OrderChangeNotification",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "WorkOrder",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldDefaultValue: "NotGenerated");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ChangeType",
+                table: "OrderChangeNotification",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
